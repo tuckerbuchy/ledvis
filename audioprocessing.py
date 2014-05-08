489cb7b54a2d1b16c23c21adbceecf7727dd49a0
g9c7 <tukrre@localhost.localdomain>
2014-05-08 00:11:55 -0700

    refactoring

--- audioprocessing.py
+++ audioprocessing.py
@@ -4,7 +4,8 @@ import numpy
 import array
 import matplotlib.pyplot as plt
 import serial
-import Tkinter
+from Tkinter import *
+#   import tkMessageBox
 import math
 import colorsys
 import matplotlib
@@ -17,152 +18,197 @@ class IIR:
     def update(self, value):
         self.prev = (1-self.alpha)*value + self.alpha*self.prev
         return self.prev
+
+class SoundToColorProcessor:
+    def __init__(self):
+        ##################CONSTANTS####################
+        self.HSV_VALUE = 0.7
+
+        self.PEAK_THRESHOLD = 3e6
+        self.BAND_LOWER = 40
+        self.BAND_UPPER = 500
+
+        self.SATURATION_FIR_DEQUE_SIZE = 20
+        self.HUE_IIR_ALPHA = 0.9
+
+        self.CHUNK = 2048
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 2
+        self.RATE = 44100
+
+        self.OCTAVES = 1
+        ###############################################
+    '''
+    calculate the magnitude of the fourier tranform parts
+    '''
+    def calculateMagnitude(self, real, imaginary):
+        magnitudes = [] #where we wills store mags
+        x = 0
+        while x < len(real) and x < len(imaginary):
+            magnitudes.append(numpy.sqrt(real[x]*real[x] + imaginary[x]*imaginary[x]))
+            x += 1
+        return magnitudes
+
+    def getFrequencyIndex(self, freq):
+        return freq/(self.RATE/self.CHUNK)
+
+    def convertPercentToColorValue(self, percent):
+        return int(round(percent*255))
+
+    def convertColorVectorToString(self, color_vector):
+        result = ""
+        for color in color_vector:
+            result += (format(color, '02x'))
+        return result
+
+    def mapFrequencyToHue(self, freq):
+        if freq < self.BAND_LOWER:
+            freq = self.BAND_LOWER
+        elif freq > self.BAND_UPPER:
+            freq = self.BAND_UPPER
+            
+        freq_log = numpy.log(freq)/numpy.log(2**self.OCTAVES)
+        lower_log = numpy.log(self.BAND_LOWER)/numpy.log(2**self.OCTAVES)
+        upper_log = numpy.log(self.BAND_UPPER)/numpy.log(2**self.OCTAVES)
+        hue = (freq_log - lower_log) / (1.0*upper_log - 1.0*lower_log)
+        return hue
+
+    def startProcessing(self):
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=self.FORMAT,
+                        channels=self.CHANNELS,
+                        rate=self.RATE,
+                        input=True,
+                        frames_per_buffer=self.CHUNK)
+
+        #ser = serial.Serial('/dev/ttyACM0')
+        #print "SERIAL NAME: " + ser.name
+
+        ###Plots and canvas initialization########
+        plt.ion()
+        fig, ax = plt.subplots()
+        line, = ax.plot(numpy.random.randn(100))
+        plt.axis([20, 5000, 0, 1.1e7])
+        plt.show(block=False)
+        tk_canvas = Tk()
+        tk_canvas.geometry("500x500")
+
+        hue_iir = IIR(self.HUE_IIR_ALPHA)
+        saturation_fir_deque = []
+
+        while(1):
+            data = stream.read(self.CHUNK)
+            nums = array.array('h', data)
+            results = numpy.fft.fft(nums)
+            freq_bins = numpy.fft.fftfreq(len(nums), 1.0/self.RATE)
+                
+            results = results[0:(len(results)/2 - 1)]
+            freq_bins = 2 * freq_bins[0:(len(freq_bins)/2 - 1)]
+            
+            mags = self.calculateMagnitude(results.real, results.imag)
+            
+            lower_band_index = self.getFrequencyIndex(self.BAND_LOWER)
+            upper_band_index = self.getFrequencyIndex(self.BAND_UPPER)
+            
+            max_mag  = max(mags[lower_band_index:upper_band_index])
+            max_freq_index = mags.index(max_mag)
+            max_freq = freq_bins[max_freq_index]
+            
+            new_frequency = self.BAND_LOWER
+            #if max_freq >= BAND_LOWER and max_freq < BAND_UPPER:
+            if max_mag > self.PEAK_THRESHOLD:
+                new_frequency = max_freq
+            
+            averaged_frequency = hue_iir.update(new_frequency)
+            hue = self.mapFrequencyToHue(averaged_frequency)
+            
+            mag_sum = numpy.sum(mags)
+            saturation_fir_deque.append(mag_sum)
+            if len(saturation_fir_deque) > self.SATURATION_FIR_DEQUE_SIZE:
+                print("Popped!")
+                saturation_fir_deque.pop(0)
+            
+            average_sum = numpy.mean(saturation_fir_deque)
+            max_sum = max(saturation_fir_deque)
+            if max_sum == 0:
+                max_sum = 1
+            saturation = 0.85 + (0.15)*average_sum/max_sum
+            
+            rgb = colorsys.hsv_to_rgb(hue, saturation, self.HSV_VALUE)
+            rgb = map(self.convertPercentToColorValue, rgb)
+            rgb_string = "#" + self.convertColorVectorToString(rgb)
+            packet = [0,0,0]
+            packet = []
+
+            for color in rgb:
+                packet.append(color)
+            
+            #ser.write(bytearray(packet))
+            
+            print("Frequency: " + str(max_freq) + " " + "Color: " + rgb_string)
+            tk_canvas.configure(background=rgb_string)
+            
+            ##graph the fourier stuff
+            line.set_data(freq_bins, mags)
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+    def setUpperBand(self, val):
+        high = int(val)
+        if high <= self.BAND_LOWER:
+            #cannot set upper band below
+            print("CANT DO THAT!")
+            tkMessageBox.showinfo("Error", "Cannot set upper band this low")
+        else:
+            self.BAND_UPPER = int(val)
+
+    def setLowerBand(self, val):
+        low = int(val)
+        if low >= self.BAND_UPPER:
+            tkMessageBox.showinfo("Error", "Cannot set lower bound this high")
+        else:
+            self.BAND_LOWER = int(val)
+
+    def setFirQueue(self, val):
+        self.SATURATION_FIR_DEQUE_SIZE = int(val)
+
+    def setHueAlpha(self, val):
+        self.HUE_IIR_ALPHA = float(val)
+
+    def setNumberOfOctaves(self, val):
+        self.OCTAVES = int(val)
+
+    def start(self):
+        main_window = Tk()
+
+        lower_band_slide = Entry(main_window)
+        lower_button = Button(main_window, text="Set Lower Band (Hz)", command=self.setLowerBand)
+        lower_band_slide.pack()
+        lower_button.pack()
         
+        upper_band_slide = Entry(main_window)
+        upper_button = Button(main_window, text="Set Upper Band (Hz)", command=self.setUpperBand)
+        upper_band_slide.pack()
+        upper_button.pack()
 
-def getSoundRatio(freq):
-    freq_log = numpy.log(freq)/numpy.log(2**OCTAVES)
-    freq_log_next_octave = numpy.ceil(freq_log)
-    freq_log_prev_octave = numpy.floor(freq_log)
-    print("freq_log: " + str(freq_log) + " next octave: " + str(freq_log_next_octave))  
-    ratio = (freq_log - freq_log_prev_octave)
-    print("RATIO: " + str(ratio))
-    return ratio
+        num_octaves_slide = Scale(main_window, from_=1, to_=5, label="Number of Octaves", command=self.setFirQueue)
+        num_octaves_slide.pack()
 
-def calculateMagnitude(real, imaginary):
-    magnitudes = [] #where we wills store mags
-    x = 0
-    while x < len(real) and x < len(imaginary):
-        magnitudes.append(numpy.sqrt(real[x]*real[x] + imaginary[x]*imaginary[x]))
-        x += 1
-    
-    return magnitudes
+        fir_queue_slide = Scale(main_window, from_=1, to_=40, label="FIR Saturation Queue", command=self.setFirQueue)
+        fir_queue_slide.pack()
 
-def getFrequencyIndex(freq):
-    return freq/(RATE/CHUNK)
+        #iir_queue_slide = Scale(main_window, resolution_=0.01, from_=0, to_=1, label="IIR Hue Alpha", command=self.setHueAlpha)
+        #iir_queue_slide.pack()
 
-def convertPercentToColorValue(percent):
-    return int(round(percent*255))
+        self.startProcessing()
+        control_button = Button(main_window, text="Start/Pause", command=self.startProcessing)
+        control_button.pack()
 
-def convertColorVectorToString(color_vector):
-    result = ""
-    for color in color_vector:
-        result += (format(color, '02x'))
-    return result
 
-def mapFrequencyToHue(freq):
-    if freq < BAND_LOWER:
-        freq = BAND_LOWER
-    elif freq > BAND_UPPER:
-        freq = BAND_UPPER
-        
-    freq_log = numpy.log(freq)/numpy.log(2**OCTAVES)
-    lower_log = numpy.log(BAND_LOWER)/numpy.log(2**OCTAVES)
-    upper_log = numpy.log(BAND_UPPER)/numpy.log(2**OCTAVES)
-    hue = (freq_log - lower_log) / (1.0*upper_log - 1.0*lower_log)
-    return hue    
-
-##################CONSTANTS####################
-HSV_VALUE = 0.7
-
-PEAK_THRESHOLD = 3e6
-BAND_LOWER = 40
-BAND_UPPER = 500
-
-SATURATION_FIR_DEQUE_SIZE = 20
-HUE_IIR_ALPHA = 0.9
-
-CHUNK = 2048
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 1000
-
-OCTAVES = 1
-###############################################
-p = pyaudio.PyAudio()
-
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
-
-ser = serial.Serial('/dev/ttyACM0')
-print "SERIAL NAME: " + ser.name
-
-print("* recording")
-
-###Plots and canvas initialization########
-plt.ion()
-fig, ax = plt.subplots()
-line, = ax.plot(numpy.random.randn(100))
-plt.axis([20, 5000, 0, 1.1e7])
-plt.show(block=False)
-tk_canvas = Tkinter.Tk()
-tk_canvas.geometry("500x500")
-##########################################
-
-hue_iir = IIR(HUE_IIR_ALPHA)
-saturation_fir_deque = []
-
-while(1):
-    data = stream.read(CHUNK)
-    nums = array.array('h', data)
-    results = numpy.fft.fft(nums)
-    freq_bins = numpy.fft.fftfreq(len(nums), 1.0/RATE)
-        
-    results = results[0:(len(results)/2 - 1)]
-    freq_bins = 2 * freq_bins[0:(len(freq_bins)/2 - 1)]
-    
-    mags = calculateMagnitude(results.real, results.imag)
-    
-    lower_band_index = getFrequencyIndex(BAND_LOWER)
-    upper_band_index = getFrequencyIndex(BAND_UPPER)
-    
-    max_mag  = max(mags[lower_band_index:upper_band_index])
-    max_freq_index = mags.index(max_mag)
-    max_freq = freq_bins[max_freq_index]
-    
-    new_frequency = BAND_LOWER
-    #if max_freq >= BAND_LOWER and max_freq < BAND_UPPER:
-    if max_mag > PEAK_THRESHOLD:
-        new_frequency = max_freq
-    
-    averaged_frequency = hue_iir.update(new_frequency)
-    hue = mapFrequencyToHue(averaged_frequency)
-    
-    mag_sum = numpy.sum(mags)
-    saturation_fir_deque.append(mag_sum)
-    if len(saturation_fir_deque) > SATURATION_FIR_DEQUE_SIZE:
-        print("Popped!")
-        saturation_fir_deque.pop(0)
-    
-    average_sum = numpy.mean(saturation_fir_deque)
-    max_sum = max(saturation_fir_deque)
-    if max_sum == 0:
-        max_sum = 1
-    saturation = 0.85 + (0.15)*average_sum/max_sum
-    
-    rgb = colorsys.hsv_to_rgb(hue, saturation, HSV_VALUE)
-    rgb = map(convertPercentToColorValue, rgb)
-    rgb_string = "#" + convertColorVectorToString(rgb)
-    packet = [0,0,0]
-    packet = []
-
-    for color in rgb:
-        packet.append(color)
-    
-    ser.write(bytearray(packet))
-    
-    print("Frequency: " + str(max_freq) + " " + "Color: " + rgb_string)
-    tk_canvas.configure(background=rgb_string)
-    
-    ##graph the fourier stuff
-    line.set_data(freq_bins, mags)
-    fig.canvas.draw()
-    fig.canvas.flush_events()
-print("* done recording")
-
-stream.stop_stream()
-stream.close()
-p.terminate()
+proc = SoundToColorProcessor()
+proc.start();
